51a34f23705a694ed53023973ae35f6433c1a7c0
Jackalob <nike9712@gmail.com>
2022-04-13 01:30:02 +0800

    add content of Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>This is medium 2.0</h1>
-    </div>
+      <Header />
+    </div>　
   )
 }
 
