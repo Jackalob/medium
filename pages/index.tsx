ea7a912589bbf40f1bd9839845c8caee6352a789
Jackalob <nike9712@gmail.com>
2022-04-27 00:58:29 +0800

    fetch data and pass it to component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,15 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from 'components/Header'
 import Banner from 'components/Banner'
+import { sanityClient, urlFor } from 'sanity.js'
+import { Post } from 'typings'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post]
+}
+
+const Home: NextPage<Props> = ({ posts }) => {
+  console.log(posts)
   return (
     <div className='max-w-7xl mx-auto'>
       <Head>
@@ -17,3 +24,24 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    slug,
+    description,
+    mainImage,
+    author -> {
+      name,
+      image
+    }
+  }`;
+  const posts = await sanityClient.fetch(query)
+  
+  return {
+    props: {
+      posts
+    }  
+  }
+}
