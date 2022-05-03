107bb8f7bf40e5428831bd1178f0e252cadd2ad1
Jackalob <nike9712@gmail.com>
2022-05-03 09:48:16 +0800

    add comment block

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -49,6 +49,26 @@ const Post = ({ post }: Props) => {
           />
         </div>
       </article>
+
+      <hr className="max-w-lg my-5  mx-auto border border-yellow-500"/>
+
+      <form className='flex flex-col p-5 max-w-2xl mx-auto mb-10'>
+        <h3 className='text-sm text-yellow-500'>Enjoyed this article?</h3>
+        <h4 className='text-3xl font-bold'>Leave a comment below!</h4>
+        <hr className='py-3 mt-2' />
+        <label className='block mb-5'>
+          <span className='text-gray-700'>Name</span>
+          <input className='shadow border rounded py-2 px-3 mt-1 block w-full outline-none focus:ring ring-yellow-500' type="text" placeholder="ttt" />
+        </label>
+        <label className='block mb-5'>
+          <span className='text-gray-700'>Email</span>
+          <input className='shadow border rounded py-2 px-3 mt-1 block w-full outline-none focus:ring ring-yellow-500' type="text" placeholder="ttt" />
+        </label>
+        <label className='block mb-5'>
+          <span className='text-gray-700'>Comment</span>
+          <textarea className='shadow border rounded py-2 px-3 mt-1 block w-full outline-none focus:ring ring-yellow-500' rows={8} placeholder="ttt" />
+        </label>
+      </form>
     </main>
   )
 }
