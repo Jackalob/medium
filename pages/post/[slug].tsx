e9033e3b1ca39104dec8a291efddf479e3041037
Jackalob <nike9712@gmail.com>
2022-05-05 23:59:02 +0800

    add comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -141,6 +141,20 @@ const Post = ({ post }: Props) => {
           className='shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer'
         />
       </form>
+
+      {/* Comments */}
+      <div className='flex flex-col p-10 my-10 max-w-2xl mx-auto shadow space-y-2'>
+        <h3 className='text-4xl'>Comments</h3>
+        <hr className='pb-2' />
+        {post.comments.map(comment => (
+          <div key={comment._id}>
+            <p>
+              <span className='text-yellow-500'>{comment.name}: </span>
+              {comment.comment}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
